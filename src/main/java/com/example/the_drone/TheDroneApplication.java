b609446bc7158d6d503fd5c0f1963a75c53faf92
Akindele Odedoyin <akindeleodedoyin@gmail.com>
2025-01-05 21:25:22 +0100

    first commit after bootstrapping code

---
+++ src/main/java/com/example/the_drone/TheDroneApplication.java
@@ -0,0 +1,13 @@
+package com.example.the_drone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDroneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/the_drone/TheDroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_drone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
